d52df5dd749a658b46c9a57265101fa22e0c562c
Imad Jakhrouti <imadjakhrouti@gmail.com>
2024-11-30 15:01:51 +0100

    Add button click handling for adding and viewing recipes in MainActivity

--- app/src/main/java/ma/ump/eniad/recipeapp/MainActivity.java
+++ app/src/main/java/ma/ump/eniad/recipeapp/MainActivity.java
@@ -1,6 +1,8 @@
 package ma.ump.eniad.recipeapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +17,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        Button ajouter_recette = findViewById(R.id.ajouter_recette);
+        Button consulter_recettes = findViewById(R.id.consulter_recettes);
+
+        ajouter_recette.setOnClickListener(view -> {
+            startActivity(new Intent(getApplicationContext(), AddRecipeActivity.class));
+        });
+
+        consulter_recettes.setOnClickListener(view -> {
+            startActivity(new Intent(getApplicationContext(), ViewRecipesActivity.class));
+        });
+
+
+
+
+
+
+
+
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
